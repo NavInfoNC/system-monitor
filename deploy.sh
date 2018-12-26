67befc679aabbc0f3201d122cec73da98c9e1424
liudf <liudf@mapbar.com>
2018-12-26 11:13:45 +0800

    tweak

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,9 @@ fi
 echo dst: $dst
 
 find $dir -iname "*.py" | xargs chmod +x > /dev/null 2>&1
-
 rsync -avP *.py $dst
+rsync -avP html/*.html $dst/html/
+
+if [[ $HOSTNAME == 'wl-load-test' ]];then
+	chgrp ncserver $dst/* > /dev/null 2>&1
+fi
