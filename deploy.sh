10ca4b6b0a100a6548bf701fcd07e7793477fb76
liudf <liudf@mapbar.com>
2018-12-26 20:13:29 +0800

    update html

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ echo dst: $dst
 
 find $dir -iname "*.py" | xargs chmod +x > /dev/null 2>&1
 rsync -avP *.py $dst
-rsync -avP html/*.html $dst/html/
+rsync -avP html/* $dst/html/
 
 if [[ $HOSTNAME == 'wl-load-test' ]];then
 	chgrp ncserver $dst/* > /dev/null 2>&1
