be1e60fea95bf8ca0bae051e0bccbab73eced799
zhaoguangqiang <zhaoguangqiang12@gmail.com>
2019-03-05 11:23:20 +0800

    modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-if [[ $HOSTNAME == 'wl-load-test' ]];then
-	dst=/etc/ncserver/system-monitor
-	mkdir -p $dst
-elif [[ $HOSTNAME == 'wl-server-build' ]];then
+if [[ $HOSTNAME == 'wl-server-build' ]];then
 	dst=$USER@navicore.mapbar.com:/etc/ncserver/system-monitor
 else
-	echo "can't support auto deploy for your MACHINE"
-	exit
+	dst=/etc/ncserver/system-monitor
+	mkdir -p $dst
 fi
 
 
